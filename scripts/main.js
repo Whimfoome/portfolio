c8862ce7a592e2fb9cb02a191b88a43b173c76ec
Dimitar Dimitrov <42871796+Whimfoome@users.noreply.github.com>
2022-05-31 12:39:36 +0300

    Added new mobile project

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
-let womOpened = false;
+let womOpened = true;
+let fmanimeOpened = true;
+
+wom_collapse();
+fmanime_collapse();
 
 function wom_collapse() {
-    let moreText = document.getElementById("more");
+    let moreText = document.getElementById("more-wom");
     let btnText = document.getElementById("womBtn");
   
     if (womOpened === true) {
@@ -10,6 +14,21 @@ function wom_collapse() {
       moreText.style.display = "none";
     } else {
         womOpened = true;
+        btnText.innerHTML = "<b>Read Less</b>";
+        moreText.style.display = "inline";
+    }
+} 
+
+function fmanime_collapse() {
+    let moreText = document.getElementById("more-fmanime");
+    let btnText = document.getElementById("fmanimeBtn");
+  
+    if (fmanimeOpened === true) {
+        fmanimeOpened = false;
+      btnText.innerHTML = "<b>Read More</b>";
+      moreText.style.display = "none";
+    } else {
+        fmanimeOpened = true;
         btnText.innerHTML = "<b>Read Less</b>";
         moreText.style.display = "inline";
     }
