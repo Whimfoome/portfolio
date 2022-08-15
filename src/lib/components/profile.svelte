812c67dba5ae5a366f774b4645ac44236306cd84
Dimitar Dimitrov <42871796+Whimfoome@users.noreply.github.com>
2022-08-15 16:32:06 +0300

    Profile Section is done

---
+++ src/lib/components/profile.svelte
@@ -0,0 +1,66 @@
+<script>
+	import avatar from '$lib/assets/avatar.png';
+	import logo_github from '$lib/assets/logo-github.png';
+	import logo_twitter from '$lib/assets/logo-twitter.png';
+	import logo_youtube from '$lib/assets/logo-youtube.png';
+	import logo_linkedin from '$lib/assets/logo-linkedin.png';
+</script>
+
+<div class="profile">
+	<img class="avatar" alt="Avatar" src={avatar} />
+	<h2 class="name">Dimitar Dimitrov</h2>
+	<h3 class="username">Whimfoome</h3>
+	<p class="description">
+		I like making games
+		<br /><br />
+		Experienced with Unreal Engine, Godot and a bit of Unity
+		<br /><br />
+		Love contributing to open-source projects
+	</p>
+	<div class="social">
+		<a href="https://github.com/Whimfoome" target="_blank">
+			<img src={logo_github} alt="GitHub" />
+		</a>
+		<a href="https://twitter.com/Whimfoome1" target="_blank">
+			<img src={logo_twitter} alt="Twitter" />
+		</a>
+		<a href="https://www.youtube.com/channel/UC-st0HrCRVScIW75sCGpH_Q" target="_blank">
+			<img src={logo_youtube} alt="YouTube" />
+		</a>
+		<a href="https://www.linkedin.com/in/dimitar-dimitrov-9a7730235" target="_blank">
+			<img src={logo_linkedin} alt="LinkedIn" />
+		</a>
+	</div>
+</div>
+
+<style>
+	.profile {
+		margin: 3rem;
+	}
+
+	.avatar {
+		width: 16.25rem;
+		height: 16.25rem;
+		border-radius: 50%;
+	}
+
+	.name {
+		color: white;
+		margin-top: 1.25rem;
+		margin-bottom: 0rem;
+	}
+
+	.username {
+		margin: 0rem;
+		font-weight: 300;
+	}
+
+	.description {
+		color: white;
+		padding-bottom: 0.65em;
+	}
+
+	a {
+		padding-right: 0.5em;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '$lib/global.css';
+	import Profile from '$lib/components/profile.svelte';
+</script>
+
+<div class="grid-container">
+	<Profile />
+	<p>aaa</p>
+</div>
+
+<style>
+	:global(body) {
+		background-color: #1a202c;
+		color: #cbd5e0;
+		font: inherit;
+		font-family: Fira Code;
+		font-weight: normal;
+		font-style: normal;
+	}
+	.grid-container {
+		display: grid;
+		grid-template-columns: 1fr 2fr;
+	}
+</style>
