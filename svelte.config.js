5e4cd8d155fbb8c22b7d35ccf172a501df8cd0e2
Dimitar Dimitrov <42871796+Whimfoome@users.noreply.github.com>
2023-03-05 15:28:28 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -12,11 +10,11 @@ const config = {
 	kit: {
 		adapter: adapter({
 			pages: 'build',
-			assets: 'build'
-		}),
-		paths: {
-			base: dev ? '' : '/portfolio'
-		}
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
